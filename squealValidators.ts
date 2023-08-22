01f5e0d5b5dfb223afdd7bf907b3c87c636883b9
bugboy-20 <diegoroberto.ammirabile@gmail.com>
2023-08-22 19:58:17 +0200

    Aggiornamento squealValidators

--- squealValidators.ts
+++ squealValidators.ts
@@ -10,35 +10,24 @@ const receiversArray = z
     message: 'Non puoi inserire destinatari duplicati',
   });
 
-export const squealReadSchema = z.object({
-  _id: z.string(), // TODO: https://stackoverflow.com/questions/76284139/how-to-use-a-zod-field-has-different-validation-based-on-conditions
+export const squealWriteSchema = z.object({
   receivers: receiversArray,
   author: userString,
-  body: z.string(),
+  body: z.string().regex(/^(txt|img|vid|geo):.*/,{ message: 'Specifica il tipo di bodyche vuoi assengnare'}),  
+  category: z.array(z.string()),
+});
+
+export type squealWrite_t = z.infer<typeof squealWriteSchema>;
+
+export const squealReadSchema = z.object({
+  _id: z.string(), // TODO: https://stackoverflow.com/questions/76284139/how-to-use-a-zod-field-has-different-validation-based-on-conditions
+  ...squealWriteSchema.shape,
   datetime: z.coerce.date(),
   impressions: positiveInteger,
   positive_reaction: positiveInteger,
   negative_reaction: positiveInteger,
-  category: z.array(z.string()),
-  automatic_receivers: z
-    .array(receiverString)
-    .refine((items) => new Set(items).size === items.length, {
-      message: 'Non puoi inserire destinatari duplicati',
-    }),
-});
+  });
 
 export type squealRead_t = z.infer<typeof squealReadSchema>;
 
-export const squealWriteSchema = z.object({
-  receivers: receiversArray,
-  author: userString,
-  body: z.string(),
-  category: z.array(z.string()),
-  automatic_receiver: z
-    .array(receiverString)
-    .refine((items) => new Set(items).size === items.length, {
-      message: 'Non puoi inserire destinatari duplicati',
-    }),
-});
 
-export type squealWrite_t = z.infer<typeof squealWriteSchema>;
