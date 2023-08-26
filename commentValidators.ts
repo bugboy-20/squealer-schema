6ed7ef3105d4e089d4ebe53cd3d27441cb13444d
bugboy-20 <diegoroberto.ammirabile@gmail.com>
2023-08-26 15:54:09 +0200

    commentValidator

---
+++ commentValidators.ts
@@ -0,0 +1,15 @@
+import {z} from "zod";
+import {squealWriteSchema, squealReadSchema} from "./squealValidators";
+
+export const commentWriteSchema = squealWriteSchema
+  .omit({category: true})
+  .extend({reference: z.string()})
+
+export type commentWrite_t = z.infer<typeof commentWriteSchema>;
+
+export const commentReadSchema = squealReadSchema
+  .omit({category: true})
+  .extend({reference: z.string() })
+
+export type commentRead_t = z.infer<typeof commentReadSchema>;
+
